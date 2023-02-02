11e69b5661c33091e74e570af710e72121b6943a
Petr Babic <pbabic@redhat.com>
2023-02-02 11:10:22 +0100

    refactor based on review

--- dev/build.go
+++ dev/build.go
@@ -3,10 +3,10 @@ package dev
 import (
 	"fmt"
 	"github.com/magefile/mage/mg"
-	"github.com/magefile/mage/sh"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 type ImageBuildInfo struct {
@@ -22,42 +22,57 @@ type ImagePushInfo struct {
 	BuildInfo  *ImageBuildInfo
 }
 
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
+func execError(command []string, err error) error {
+	return fmt.Errorf("running command '%s': %w", strings.Join(command, " "), err)
+}
+
+func newExecCmd(args []string, cacheDir string) *exec.Cmd {
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Dir = cacheDir
+	return cmd
 }
 
 // BuildImage is a generic image build function,
 // requires the binaries to be built beforehand
-func BuildImage(buildInfo *ImageBuildInfo, deps []interface{}) {
+func BuildImage(buildInfo *ImageBuildInfo, deps []interface{}) error {
 	if len(deps) > 0 {
 		mg.SerialDeps(deps...)
 	}
 
-	buildCmd := []string{buildInfo.Runtime, "build", "-t", buildInfo.ImageTag}
+	buildCmdArgs := []string{buildInfo.Runtime, "build", "-t", buildInfo.ImageTag}
 	if buildInfo.ContainerFile != "" {
-		buildCmd = append(buildCmd, "-f", buildInfo.ContainerFile)
+		buildCmdArgs = append(buildCmdArgs, "-f", buildInfo.ContainerFile)
 	}
-	buildCmd = append(buildCmd, buildInfo.ContextDir)
+	buildCmdArgs = append(buildCmdArgs, buildInfo.ContextDir)
 
-	cmds := [][]string{
-		buildCmd,
+	commands := [][]string{
+		buildCmdArgs,
 		{buildInfo.Runtime, "image", "save", "-o", buildInfo.CacheDir + ".tar", buildInfo.ImageTag},
 	}
 
 	// Build image!
-	for _, command := range cmds {
-		buildCmd := exec.Command(command[0], command[1:]...)
-		buildCmd.Stderr = os.Stderr
-		buildCmd.Stdout = os.Stdout
-		buildCmd.Dir = buildInfo.CacheDir
-		must(buildCmd.Run())
+	for _, command := range commands {
+		buildCmd := newExecCmd(command, buildInfo.CacheDir)
+		if err := buildCmd.Run(); err != nil {
+			return execError(command, err)
+		}
 	}
+	return nil
+}
+
+func quayLogin(runtime, cacheDir string) error {
+	args := []string{runtime, "login", "-u=" + os.Getenv("QUAY_USER"), "-p=" + os.Getenv("QUAY_TOKEN"), "quay.io"}
+	loginCmd := newExecCmd(args, cacheDir)
+	if err := loginCmd.Run(); err != nil {
+		return execError(args, err)
+	}
+	return nil
 }
 
 // PushImage builds and pushes only the given container image to the default registry.
-func PushImage(pushInfo *ImagePushInfo, deps []interface{}) {
+func PushImage(pushInfo *ImagePushInfo, deps []interface{}) error {
 	mg.SerialDeps(mg.F(BuildImage, pushInfo.BuildInfo, deps))
 
 	// Login to container registry when running on AppSRE Jenkins.
@@ -65,19 +80,20 @@ func PushImage(pushInfo *ImagePushInfo, deps []interface{}) {
 	_, isCI := os.LookupEnv("CI")
 	if isJenkins || isCI {
 		log.Println("running in CI, calling container runtime login")
-		args := []string{"login", "-u=" + os.Getenv("QUAY_USER"), "-p=" + os.Getenv("QUAY_TOKEN"), "quay.io"}
-		if err := sh.Run(pushInfo.BuildInfo.Runtime, args...); err != nil {
-			panic(fmt.Errorf("registry login: %w", err))
+		if err := quayLogin(pushInfo.BuildInfo.Runtime, pushInfo.BuildInfo.CacheDir); err != nil {
+			return err
 		}
 	}
 
-	args := []string{"push"}
+	args := []string{pushInfo.BuildInfo.Runtime, "push"}
 	if pushInfo.BuildInfo.Runtime == string(ContainerRuntimePodman) {
 		args = append(args, "--digestfile="+pushInfo.DigestFile)
 	}
 	args = append(args, pushInfo.BuildInfo.ImageTag)
 
-	if err := sh.Run(pushInfo.BuildInfo.Runtime, args...); err != nil {
-		panic(fmt.Errorf("pushing image: %w", err))
+	pushCmd := newExecCmd(args, pushInfo.BuildInfo.CacheDir)
+	if err := pushCmd.Run(); err != nil {
+		return execError(args, err)
 	}
+	return nil
 }
